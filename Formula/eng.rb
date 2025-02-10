fc9aaf2da0278e2d0767d8826b002bb2ae051a70
Eric N. Garcia <eng618@garciaericn.com>
2025-02-10 11:59:06 -0500

    feat(formula): update completion installation method

--- Formula/eng.rb
+++ Formula/eng.rb
@@ -5,19 +5,15 @@ class Eng < Formula
   sha256 "2e0df748693261740eb62d8ecc2d88af11c019ce107f38ab832556e1ea9fc085"
   license "MIT"
 
-depends_on "go" => :build
+  depends_on "go" => :build
 
   def install
     system "go", "build",  "-o", bin/"eng"
 
     # Install shell completions
-    generate_completions
-  end
-
-  def generate_completions
-    (bash_completion/"eng").write Utils.safe_popen_read("#{bin}/eng", "completion", "bash")
-    (zsh_completion/"_eng").write Utils.safe_popen_read("#{bin}/eng", "completion", "zsh")
-    (fish_completion/"eng.fish").write Utils.safe_popen_read("#{bin}/eng", "completion", "fish")
+    bash_completion.install "completion/eng.bash" => "eng"
+    zsh_completion.install "completion/eng.zsh"   => "_eng"
+    fish_completion.install "completion/eng.fish" => "eng.fish"
   end
 
   test do
