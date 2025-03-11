9581d0e932f46fa6421e0fee0fdc8a4ef026bcda
Eric N. Garcia <eng618@garciaericn.com>
2025-03-11 17:56:01 +0000

    Update eng to v0.15.4

--- Formula/eng.rb
+++ Formula/eng.rb
@@ -1,26 +1,26 @@
 class Eng < Formula
-  desc "Personal cli to help facilitate my normal workflow"
-  homepage "https://github.com/eng618/homebrew-eng"
-  url "https://github.com/eng618/eng/archive/refs/tags/v0.14.3.tar.gz"
-  sha256 "6aa7a1e75b6279a2ad8b0795870d973182d0641b20caae747334a554884956cf"
-  license "MIT"
+  desc 'Personal cli to help facilitate my normal workflow'
+  homepage 'https://github.com/eng618/eng'
+  url 'https://github.com/eng618/eng/releases/download/v0.15.4/eng_darwin_amd64.tar.gz'
+  sha256 ''
+  license 'MIT'
 
-  depends_on "go" => :build
+  depends_on 'go' => :build
 
   def install
-    system "go", "build",  "-o", bin/"eng"
+    system 'go', 'build',  '-o', bin/'eng'
 
     # Install shell completions
     generate_completions
   end
 
   def generate_completions
-    (bash_completion/"eng").write Utils.safe_popen_read("#{bin}/eng", "completion", "bash")
-    (zsh_completion/"_eng").write Utils.safe_popen_read("#{bin}/eng", "completion", "zsh")
-    (fish_completion/"eng.fish").write Utils.safe_popen_read("#{bin}/eng", "completion", "fish")
+    (bash_completion/'eng').write Utils.safe_popen_read('#{bin}/eng', 'completion', 'bash')
+    (zsh_completion/'_eng').write Utils.safe_popen_read('#{bin}/eng', 'completion', 'zsh')
+    (fish_completion/'eng.fish').write Utils.safe_popen_read('#{bin}/eng', 'completion', 'fish')
   end
 
   test do
-    system "#{bin}/eng", "--version"
+    system '#{bin}/eng', '--help'
   end
 end
