4ceaf3e6102ca16dfa71e2fa59bb9ec58d186c4d
Eric N. Garcia <eng618@garciaericn.com>
2025-04-04 07:08:35 +0000

    Update eng to v0.16.14

--- Formula/eng.rb
+++ Formula/eng.rb
@@ -3,42 +3,42 @@ class Eng < Formula
   homepage 'https://github.com/eng618/eng'
   case
   when OS.mac? && Hardware::CPU.intel?
-    url 'https://github.com/eng618/eng/releases/download/v0.16.13/eng_Darwin_x86_64.tar.gz'
-    sha256 '4238501b52dc24d3584497c40aeb0794a5e2da281b1048be9f8c6bb5133292b8'
+    url 'https://github.com/eng618/eng/releases/download/v0.16.14/eng_Darwin_x86_64.tar.gz'
+    sha256 'c1c3f52d1532c511a2fb0e8ddea54898ee6b221a515ed14a0469f0bb2fc872b3'
   when OS.mac? && Hardware::CPU.arm?
-    url 'https://github.com/eng618/eng/releases/download/v0.16.13/eng_Darwin_arm64.tar.gz'
-    sha256 '91b2a73e2502303893340dcba4b7acefd87577d5515930119721020b17b5b546'
+    url 'https://github.com/eng618/eng/releases/download/v0.16.14/eng_Darwin_arm64.tar.gz'
+    sha256 '965590a8fab50241bfd0cb910374a72bc21e7753929005971d39ab593da3a866'
   when OS.linux?
     if Hardware::CPU.intel?
-      url 'https://github.com/eng618/eng/releases/download/v0.16.13/eng_Linux_x86_64.tar.gz'
-      sha256 'b5c59b02e90269b2520b5802f4f1e97c414c7c9f95407e7b6cebe2676b515090'
+      url 'https://github.com/eng618/eng/releases/download/v0.16.14/eng_Linux_x86_64.tar.gz'
+      sha256 '63df2592aced1858457a07088d6e1499abc463fe1296a09f0974ac28a67aaa28'
     elsif Hardware::CPU.arm?
-      url 'https://github.com/eng618/eng/releases/download/v0.16.13/eng_Linux_arm64.tar.gz'
-      sha256 'ca924e202529b8c53b449e7f525371fbd83a1109d99f66ff2f73cc4bb29d2507'
+      url 'https://github.com/eng618/eng/releases/download/v0.16.14/eng_Linux_arm64.tar.gz'
+      sha256 '16e17205bea03bd2e07c683a85fb81b2e452f79077719231e88e2321739b8e7e'
     end
   end
   license 'MIT'
 
   def install
-    puts "bin: \#{bin}"
-    puts "Installing eng to: \#{bin}"
+    puts "bin: #{bin}"
+    puts "Installing eng to: #{bin}"
     bin.install 'eng'
     puts "eng installed successfully"
-    puts "Permissions of eng: \#{File.stat("\#{bin}/eng").mode.to_s(8)}"
+    puts "Permissions of eng: #{File.stat("#{bin}/eng").mode.to_s(8)}"
     # Verify the binary is functional before generating completions
-    system "\#{bin}/eng", '--version'
+    system "#{bin}/eng", '--version'
     generate_completions
   end
 
   def generate_completions
-    puts "PATH: \#{ENV['PATH']}"
-    puts "Running: \#{bin}/eng completion bash"
-    (bash_completion/'eng').write Utils.safe_popen_read("\#{bin}/eng", 'completion', 'bash')
-    (zsh_completion/'_eng').write Utils.safe_popen_read("\#{bin}/eng", 'completion', 'zsh')
-    (fish_completion/'eng.fish').write Utils.safe_popen_read("\#{bin}/eng", 'completion', 'fish')
+    puts "PATH: #{ENV['PATH']}"
+    puts "Running: #{bin}/eng completion bash"
+    (bash_completion/'eng').write Utils.safe_popen_read("#{bin}/eng", 'completion', 'bash')
+    (zsh_completion/'_eng').write Utils.safe_popen_read("#{bin}/eng", 'completion', 'zsh')
+    (fish_completion/'eng.fish').write Utils.safe_popen_read("#{bin}/eng", 'completion', 'fish')
   end
 
   test do
-    system "\#{bin}/eng", '--help'
+    system "#{bin}/eng", '--help'
   end
 end
