0be33241503fde2ed972e0d5f3dd25b78bb778b7
Eric N. Garcia <eng618@garciaericn.com>
2025-04-04 04:59:56 +0000

    Update eng to v0.16.0

--- Formula/eng.rb
+++ Formula/eng.rb
@@ -3,22 +3,22 @@ class Eng < Formula
   homepage 'https://github.com/eng618/eng'
   if OS.mac?
     if Hardware::CPU.intel?
-      url 'https://github.com/eng618/eng/releases/download/v0.15.13/eng_Darwin_x86_64.tar.gz'
-      sha256 'c284f1b7690682e61e733047f3e5a87f7a552e8ab3c5dc35a45a1ea68a0bb847'
+      url 'https://github.com/eng618/eng/releases/download/v0.16.0/eng_Darwin_x86_64.tar.gz'
+      sha256 ''
     elsif Hardware::CPU.arm?
-      url 'https://github.com/eng618/eng/releases/download/v0.15.13/eng_Darwin_arm64.tar.gz'
-      sha256 'f92681ecbd3bec781bd3ae97bac419072f001d1ec1fc77affc236428d621f9f2'
+      url 'https://github.com/eng618/eng/releases/download/v0.16.0/eng_Darwin_arm64.tar.gz'
+      sha256 ''
     end
   elsif OS.linux?
-    url 'https://github.com/eng618/eng/releases/download/v0.15.13/eng_Linux_arm64.tar.gz'
-    sha256 '9ec0ae517e490f24d4c530481bb32335f36a4e020fabce8ab4f62e1303cfe8b5'
+    url 'https://github.com/eng618/eng/releases/download/v0.16.0/eng_Linux_arm64.tar.gz'
+    sha256 ''
   end
   license 'MIT'
 
   depends_on 'go' => :build
 
   def install
-    system 'go', 'build', '-o', bin/'eng'
+    bin.install 'eng'
 
     # Install shell completions
     generate_completions
