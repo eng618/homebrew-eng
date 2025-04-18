8baa6400c99b8ec1c7c8c3dff9e97322f439ba84
Eric N. Garcia <eng618@garciaericn.com>
2025-04-18 05:11:25 +0000

    Update eng to v0.17.5

--- Formula/eng.rb
+++ Formula/eng.rb
@@ -1,21 +1,21 @@
 class Eng < Formula
   desc 'Personal cli to help facilitate my normal workflow'
   homepage 'https://github.com/eng618/eng'
-  # URLs now include the version in the filename
+  # URLs now use TAG_NAME (with v) for path, and FILE variable (without v) for filename
   case
   when OS.mac? && Hardware::CPU.intel?
-    url 'https://github.com/eng618/eng/releases/download/v0.17.4/eng_v0.17.4_Darwin_x86_64.tar.gz'
-    sha256 ''
+    url 'https://github.com/eng618/eng/releases/download/v0.17.5/eng_0.17.5_Darwin_x86_64.tar.gz'
+    sha256 'b431187937fdb4540bb26c370a4100e8456f8d2be54961675903fbf7c1556040'
   when OS.mac? && Hardware::CPU.arm?
-    url 'https://github.com/eng618/eng/releases/download/v0.17.4/eng_v0.17.4_Darwin_arm64.tar.gz'
-    sha256 ''
+    url 'https://github.com/eng618/eng/releases/download/v0.17.5/eng_0.17.5_Darwin_arm64.tar.gz'
+    sha256 'da87602068245d74fc34f98ab3b1b87eb83628ab6378c2c1c4751430479a9e7f'
   when OS.linux?
     if Hardware::CPU.intel?
-      url 'https://github.com/eng618/eng/releases/download/v0.17.4/eng_v0.17.4_Linux_x86_64.tar.gz'
-      sha256 ''
+      url 'https://github.com/eng618/eng/releases/download/v0.17.5/eng_0.17.5_Linux_x86_64.tar.gz'
+      sha256 '71d2eea759e00bc8a59f1867b4e13ebfd20868d8e19d446ddd4a413d8c315231'
     elsif Hardware::CPU.arm?
-      url 'https://github.com/eng618/eng/releases/download/v0.17.4/eng_v0.17.4_Linux_arm64.tar.gz'
-      sha256 ''
+      url 'https://github.com/eng618/eng/releases/download/v0.17.5/eng_0.17.5_Linux_arm64.tar.gz'
+      sha256 '02a77833e42e2c2d9bea6c640f754297b35a4b1cfa52fbe9593d896754fe90c4'
     end
   end
   license 'MIT'
