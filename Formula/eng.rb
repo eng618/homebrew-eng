b2c1e1abfb56684bbf0d22574eb726bfff6ecae8
Eric N. Garcia <eng618@garciaericn.com>
2025-04-29 14:54:06 +0000

    Update eng to v0.17.15

--- Formula/eng.rb
+++ Formula/eng.rb
@@ -1,22 +1,22 @@
 class Eng < Formula
   desc 'Personal cli to help facilitate my normal workflow'
   homepage 'https://github.com/eng618/eng'
-  version '0.17.14'
+  version '0.17.15'
   # URLs now use TAG_NAME (with v) for path, and FILE variable (without v) for filename
   case
   when OS.mac? && Hardware::CPU.intel?
-    url 'https://github.com/eng618/eng/releases/download/v0.17.14/eng_0.17.14_Darwin_x86_64.tar.gz'
-    sha256 '57c322ffab05f1c5b5088ef95a92850c1f819ce5b00cb3c861cf4efe81af908f'
+    url 'https://github.com/eng618/eng/releases/download/v0.17.15/eng_0.17.15_Darwin_x86_64.tar.gz'
+    sha256 '6cb09839ccad03e7952cb604f3bc99446ac492cd08733f43198886efcf48b6a1'
   when OS.mac? && Hardware::CPU.arm?
-    url 'https://github.com/eng618/eng/releases/download/v0.17.14/eng_0.17.14_Darwin_arm64.tar.gz'
-    sha256 'e94c9a8bb0333769295762edc651d23de34a2a1dddaae099cd834470e243bc81'
+    url 'https://github.com/eng618/eng/releases/download/v0.17.15/eng_0.17.15_Darwin_arm64.tar.gz'
+    sha256 '2745bdda8ad04c0ed75422466b57d440378fb233cf336a2555c374aa8ad01d4c'
   when OS.linux?
     if Hardware::CPU.intel?
-      url 'https://github.com/eng618/eng/releases/download/v0.17.14/eng_0.17.14_Linux_x86_64.tar.gz'
-      sha256 '36a2b0fba3c621075ad9de648d3775066415ce1a00f95f93faab625bf0979750'
+      url 'https://github.com/eng618/eng/releases/download/v0.17.15/eng_0.17.15_Linux_x86_64.tar.gz'
+      sha256 '068fec2e77f6bf195e6789fad92bd81bebb3ddb408eb3f7b256fe0b8e6613106'
     elsif Hardware::CPU.arm?
-      url 'https://github.com/eng618/eng/releases/download/v0.17.14/eng_0.17.14_Linux_arm64.tar.gz'
-      sha256 '5deca34fb934d4dac83a012507e2b5cfe62bc44d1091e28b01cd135e2bfe6fc5'
+      url 'https://github.com/eng618/eng/releases/download/v0.17.15/eng_0.17.15_Linux_arm64.tar.gz'
+      sha256 'ff5287adab9dfbb763895d115b749fc8b306e65d2226d343a9df12bab722c5b5'
     end
   end
   license 'MIT'
