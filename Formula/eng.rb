15b7f4b2e1348360001958a4f30f3f8e4dd1df5f
Eric N. Garcia <eng618@garciaericn.com>
2025-05-02 00:25:13 +0000

    Update eng to v0.19.7

--- Formula/eng.rb
+++ Formula/eng.rb
@@ -1,22 +1,22 @@
 class Eng < Formula
   desc 'Personal cli to help facilitate my normal workflow'
   homepage 'https://github.com/eng618/eng'
-  version '0.19.6'
+  version '0.19.7'
   # URLs now use TAG_NAME (with v) for path, and FILE variable (without v) for filename
   case
   when OS.mac? && Hardware::CPU.intel?
-    url 'https://github.com/eng618/eng/releases/download/v0.19.6/eng_0.19.6_Darwin_x86_64.tar.gz'
-    sha256 '2dc5927604e19bbe29640fb706dbc0b7d6af3cc2cca8fb415e2bc36a9279889b'
+    url 'https://github.com/eng618/eng/releases/download/v0.19.7/eng_0.19.7_Darwin_x86_64.tar.gz'
+    sha256 'a5348ea6292abb03f9611a15f5be29005c835bfdab35e77308a0b6db3f9121b3'
   when OS.mac? && Hardware::CPU.arm?
-    url 'https://github.com/eng618/eng/releases/download/v0.19.6/eng_0.19.6_Darwin_arm64.tar.gz'
-    sha256 '423e23016c6bd71cc9f4f173230f981c78bc98460797ece567f942485dc2f0f7'
+    url 'https://github.com/eng618/eng/releases/download/v0.19.7/eng_0.19.7_Darwin_arm64.tar.gz'
+    sha256 '595caace1495e19e471635a51b37a3634830a387d4f3ce7eb78dec2ca8c632f8'
   when OS.linux?
     if Hardware::CPU.intel?
-      url 'https://github.com/eng618/eng/releases/download/v0.19.6/eng_0.19.6_Linux_x86_64.tar.gz'
-      sha256 '793a6249119ea56fcbb19b2343f9bc3d4cb05add94cb06e4f8817823d7bf4a07'
+      url 'https://github.com/eng618/eng/releases/download/v0.19.7/eng_0.19.7_Linux_x86_64.tar.gz'
+      sha256 '29a20f7dd59780d75bc3c69ca2c2221cd8a3d8799a22ec2413c2bdaaed1e7523'
     elsif Hardware::CPU.arm?
-      url 'https://github.com/eng618/eng/releases/download/v0.19.6/eng_0.19.6_Linux_arm64.tar.gz'
-      sha256 '3f8943c3b81f4a546fe33bb8cea94e7d98cc55fab11cfd438a4d1997d704e901'
+      url 'https://github.com/eng618/eng/releases/download/v0.19.7/eng_0.19.7_Linux_arm64.tar.gz'
+      sha256 'b32d2c3d8a08624b4731b47e063906420a26623892aa1024c9973cc9683bdcc8'
     end
   end
   license 'MIT'
