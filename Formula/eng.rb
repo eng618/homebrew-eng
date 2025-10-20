676657bb9ab61ef60e8b891f2f5cd53b27154fa7
Eric N. Garcia <eng618@garciaericn.com>
2025-10-20 15:31:57 +0000

    Update eng to v0.25.13

--- Formula/eng.rb
+++ Formula/eng.rb
@@ -1,22 +1,22 @@
 class Eng < Formula
   desc 'Personal cli to help facilitate my normal workflow'
   homepage 'https://github.com/eng618/eng'
-  version '0.25.12'
+  version '0.25.13'
   # URLs now use TAG_NAME (with v) for path, and FILE variable (without v) for filename
   case
   when OS.mac? && Hardware::CPU.intel?
-    url 'https://github.com/eng618/eng/releases/download/v0.25.12/eng_0.25.12_Darwin_x86_64.tar.gz'
-    sha256 '738489fb17fe4d01b3ec0713d663512e7498754ca75f4df89064425ea6616b3b'
+    url 'https://github.com/eng618/eng/releases/download/v0.25.13/eng_0.25.13_Darwin_x86_64.tar.gz'
+    sha256 'b66eae36f1d83542409fbb75f3951397c5ac1aa20c34e4a455fb5a27c24acb82'
   when OS.mac? && Hardware::CPU.arm?
-    url 'https://github.com/eng618/eng/releases/download/v0.25.12/eng_0.25.12_Darwin_arm64.tar.gz'
-    sha256 '0e1fe4a7010dc6fe490c2a308ce75f884f8dcbb7fd933682384ff684e3f27f3f'
+    url 'https://github.com/eng618/eng/releases/download/v0.25.13/eng_0.25.13_Darwin_arm64.tar.gz'
+    sha256 '6407be16b51f1a00266847bb3389eaaa3e8ba61b18fd1856606fffb08e259f18'
   when OS.linux?
     if Hardware::CPU.intel?
-      url 'https://github.com/eng618/eng/releases/download/v0.25.12/eng_0.25.12_Linux_x86_64.tar.gz'
-      sha256 '1634bf7b476934cad7c1de4b1707c6b036ca0aab4238c39102113fa97011bb07'
+      url 'https://github.com/eng618/eng/releases/download/v0.25.13/eng_0.25.13_Linux_x86_64.tar.gz'
+      sha256 '1835153da9749d4892101e0ad3b70fe3515d4fcb9a8583e28ccdcf0e8aa36f52'
     elsif Hardware::CPU.arm?
-      url 'https://github.com/eng618/eng/releases/download/v0.25.12/eng_0.25.12_Linux_arm64.tar.gz'
-      sha256 'e53116e55460d435417a52a520c6735a0d7faf8e4c9466028a23879981f0d9e3'
+      url 'https://github.com/eng618/eng/releases/download/v0.25.13/eng_0.25.13_Linux_arm64.tar.gz'
+      sha256 'e9eb25d4e8553b5866fc0d9b9c0588536ca2fe5da96cd75d9bff770f726a561f'
     end
   end
   license 'MIT'
