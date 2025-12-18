b6f53e4019115ed20ba4c11199bc0362794e3a0a
Eric N. Garcia <eng618@garciaericn.com>
2025-12-18 20:32:59 +0000

    Update eng to v0.28.10

--- Formula/eng.rb
+++ Formula/eng.rb
@@ -1,22 +1,22 @@
 class Eng < Formula
   desc 'Personal cli to help facilitate my normal workflow'
   homepage 'https://github.com/eng618/eng'
-  version '0.28.9'
+  version '0.28.10'
   # URLs now use TAG_NAME (with v) for path, and FILE variable (without v) for filename
   case
   when OS.mac? && Hardware::CPU.intel?
-    url 'https://github.com/eng618/eng/releases/download/v0.28.9/eng_0.28.9_Darwin_x86_64.tar.gz'
-    sha256 '609f4202d064e3386adbf231053459e74e9468bbe6cf5eba8433c868708cdf38'
+    url 'https://github.com/eng618/eng/releases/download/v0.28.10/eng_0.28.10_Darwin_x86_64.tar.gz'
+    sha256 '90fd0e1c242d38af43409b28733ed6b1eec07d4cbb34de2cb9376a955e7dd55b'
   when OS.mac? && Hardware::CPU.arm?
-    url 'https://github.com/eng618/eng/releases/download/v0.28.9/eng_0.28.9_Darwin_arm64.tar.gz'
-    sha256 'e03cc26f5abbc2e9d94b933e1d622410ecd20263898b9e45a8820c9e44ff1568'
+    url 'https://github.com/eng618/eng/releases/download/v0.28.10/eng_0.28.10_Darwin_arm64.tar.gz'
+    sha256 '068813ac54253fa726e0a57130bd5c4f91ac77ba56e01296e0a9ba1882aca09c'
   when OS.linux?
     if Hardware::CPU.intel?
-      url 'https://github.com/eng618/eng/releases/download/v0.28.9/eng_0.28.9_Linux_x86_64.tar.gz'
-      sha256 'af91db258c7b2dcba4787e311737057960f2866b2baf2f58bafd19227c8ae043'
+      url 'https://github.com/eng618/eng/releases/download/v0.28.10/eng_0.28.10_Linux_x86_64.tar.gz'
+      sha256 '766abd5ad98e0e36337a7644c990fa5bfeb99e12bd2ae7315174bdc56eff0013'
     elsif Hardware::CPU.arm?
-      url 'https://github.com/eng618/eng/releases/download/v0.28.9/eng_0.28.9_Linux_arm64.tar.gz'
-      sha256 '8ab2872c2e9584abac844afc42b71330244c54c65403242341af357d6469a27f'
+      url 'https://github.com/eng618/eng/releases/download/v0.28.10/eng_0.28.10_Linux_arm64.tar.gz'
+      sha256 'f099f9311c3b535b21c5a059a5bcbf16ecc4babac80945777a8514d7f7c763d9'
     end
   end
   license 'MIT'
