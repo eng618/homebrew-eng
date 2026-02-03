cc679f50fd32be87930422af799f4060cfbd80c9
Eric N. Garcia <eng618@garciaericn.com>
2026-02-03 22:12:57 +0000

    Update eng to v1.3.4

--- Formula/eng.rb
+++ Formula/eng.rb
@@ -1,22 +1,22 @@
 class Eng < Formula
   desc 'Personal cli to help facilitate my normal workflow'
   homepage 'https://github.com/eng618/eng'
-  version '1.1.1'
+  version '1.3.4'
   # URLs now use TAG_NAME (with v) for path, and FILE variable (without v) for filename
   case
   when OS.mac? && Hardware::CPU.intel?
-    url 'https://github.com/eng618/eng/releases/download/v1.1.1/eng_1.1.1_Darwin_x86_64.tar.gz'
-    sha256 'c4d14f8a63c910bf74034c9dfb14b1934dda55a4e9f2ceabd0ac5b35216e6615'
+    url 'https://github.com/eng618/eng/releases/download/v1.3.4/eng_1.3.4_Darwin_x86_64.tar.gz'
+    sha256 '0d870613916d76715f26bfef0586968ee3468bf853282d345483dccd511400fc'
   when OS.mac? && Hardware::CPU.arm?
-    url 'https://github.com/eng618/eng/releases/download/v1.1.1/eng_1.1.1_Darwin_arm64.tar.gz'
-    sha256 '5f30199b54bd363aa61cfa592d17fb73b8a11c68f0113ebb24d3f00285aaa7cc'
+    url 'https://github.com/eng618/eng/releases/download/v1.3.4/eng_1.3.4_Darwin_arm64.tar.gz'
+    sha256 'fc3abba77257884830edca7d79fdb1c072562a1ea76f51307135bc69f7bb2a3f'
   when OS.linux?
     if Hardware::CPU.intel?
-      url 'https://github.com/eng618/eng/releases/download/v1.1.1/eng_1.1.1_Linux_x86_64.tar.gz'
-      sha256 '37c7a576309f9fe8c7552b998fb5577be90ed1e1b421f59a0f5b715952641fb8'
+      url 'https://github.com/eng618/eng/releases/download/v1.3.4/eng_1.3.4_Linux_x86_64.tar.gz'
+      sha256 '7da21768a5af749f7acf21ba4c6889917f5fc2ae7f5920bf3513f6b5cb004501'
     elsif Hardware::CPU.arm?
-      url 'https://github.com/eng618/eng/releases/download/v1.1.1/eng_1.1.1_Linux_arm64.tar.gz'
-      sha256 '0e1396e9394e713b447295ba7d606232ec6e5000ec78ae0574f60e2faed1dea6'
+      url 'https://github.com/eng618/eng/releases/download/v1.3.4/eng_1.3.4_Linux_arm64.tar.gz'
+      sha256 '6f0538f4bfa4338d033d192cec54f68fbb24ddbc43df267d3c99f1a9a4a5b102'
     end
   end
   license 'MIT'
@@ -26,7 +26,7 @@ class Eng < Formula
     puts "Installing eng to: #{bin}"
     bin.install 'eng'
     puts "eng installed successfully"
-    puts "Permissions of eng: #{File.stat("#{bin}/eng").mode.to_s(8)}"
+    puts "Permissions of eng: #{File.stat(\"#{bin}/eng\").mode.to_s(8)}"
     # Verify the binary is functional before generating completions
     system "#{bin}/eng", '--help'
     generate_completions
