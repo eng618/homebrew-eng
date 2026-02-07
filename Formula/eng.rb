7d0023e1458ade0f0b3c480a531e57379d910259
Eric N. Garcia <eng618@garciaericn.com>
2026-02-07 01:36:42 +0000

    Update eng to v1.3.8

--- Formula/eng.rb
+++ Formula/eng.rb
@@ -1,22 +1,22 @@
 class Eng < Formula
   desc 'Personal cli to help facilitate my normal workflow'
   homepage 'https://github.com/eng618/eng'
-  version '1.3.6'
+  version '1.3.8'
   # URLs now use TAG_NAME (with v) for path, and FILE variable (without v) for filename
   case
   when OS.mac? && Hardware::CPU.intel?
-    url 'https://github.com/eng618/eng/releases/download/v1.3.6/eng_1.3.6_Darwin_x86_64.tar.gz'
-    sha256 'a134c62aff613753005f10ba834ff372134cc411080cb4f10048ad64d91e4b75'
+    url 'https://github.com/eng618/eng/releases/download/v1.3.8/eng_1.3.8_Darwin_x86_64.tar.gz'
+    sha256 'bfec6661a75f66ca47c004c51b9dbab935a87a048b6880fd1b6f4569bfae7055'
   when OS.mac? && Hardware::CPU.arm?
-    url 'https://github.com/eng618/eng/releases/download/v1.3.6/eng_1.3.6_Darwin_arm64.tar.gz'
-    sha256 'ace100001d400694ec49b1d5ea609e79f07773003df87b5c07e5c2d70eeca208'
+    url 'https://github.com/eng618/eng/releases/download/v1.3.8/eng_1.3.8_Darwin_arm64.tar.gz'
+    sha256 'cd19da431a5158ff0cd6fd35aea54e836d3c4dd731169c53478cde6bb365220c'
   when OS.linux?
     if Hardware::CPU.intel?
-      url 'https://github.com/eng618/eng/releases/download/v1.3.6/eng_1.3.6_Linux_x86_64.tar.gz'
-      sha256 'b03dee4245c1d9a167a03b889623580f66814541477d43a377db746802776784'
+      url 'https://github.com/eng618/eng/releases/download/v1.3.8/eng_1.3.8_Linux_x86_64.tar.gz'
+      sha256 '228c5e7691b90c0ba7f2396ace412dde6c80960e96953f7b005a1b3fb7fc6861'
     elsif Hardware::CPU.arm?
-      url 'https://github.com/eng618/eng/releases/download/v1.3.6/eng_1.3.6_Linux_arm64.tar.gz'
-      sha256 '9f5154256574b4018c0f370195fa4cb752dbb3677e9985fa1c417156df8cfebe'
+      url 'https://github.com/eng618/eng/releases/download/v1.3.8/eng_1.3.8_Linux_arm64.tar.gz'
+      sha256 '92e432a6ce3a3ba3554b500d2cdf3f68c66e153c7859c76a156bbd966f859556'
     end
   end
   license 'MIT'
